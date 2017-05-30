0b6c650099875dd32cf8b03868fec37120aaf7fc
erikdab <erik.d.burnell@gmail.com>
2017-05-30 10:45:51 +0200

    Cleaned up test for cli.main.
    * Set flake8 max-line-length to 99 in tox.ini

--- tests/test_pyrelaxmapper.py
+++ tests/test_pyrelaxmapper.py
@@ -13,31 +13,19 @@ import pytest
 from contextlib import contextmanager
 from click.testing import CliRunner
 
-from pyrelaxmapper import pyrelaxmapper
-from pyrelaxmapper import cli
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+from pyrelaxmapper import __version__, cli
 
 
 def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'pyrelaxmapper.cli.main' in result.output
+    assert 'Usage: main [OPTIONS]' in result.output
+
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert help_result.output == result.output
+
+    version_result = runner.invoke(cli.main, ['--version'])
+    assert version_result.exit_code == 0
+    assert "version {}".format(__version__) in version_result.output
