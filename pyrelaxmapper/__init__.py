cdf5b38a179c871a7147b7f5cba8b4ed1af99116
erikdab <erik.d.burnell@gmail.com>
2017-05-30 11:50:40 +0200

    Added conf directory to MANIFEST and created logger.

--- pyrelaxmapper/__init__.py
+++ pyrelaxmapper/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 __author__ = """Erik David Burnell"""
 __email__ = 'erik.d.burnell@gmail.com'
 __version__ = '0.1.0'
