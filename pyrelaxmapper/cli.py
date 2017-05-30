8157a52f86e35d688219a998dc29291c0f102d11
erikdab <erik.d.burnell@gmail.com>
2017-05-30 10:45:51 +0200

    Cleaned up test for cli.main.
    * Set flake8 max-line-length to 99 in tox.ini

--- pyrelaxmapper/cli.py
+++ pyrelaxmapper/cli.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
-
+import os
 import click
+from . import data, __version__
 
 
-@click.command()
-def main(args=None):
-    """Console script for pyrelaxmapper"""
-    click.echo("Replace this message by putting your code into "
-               "pyrelaxmapper.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.group()
+@click.version_option(version=__version__)
+def main():
+    pass
 
 
+@main.command()
+def make_dict(output):
+    """Create a cascading dictionary."""
+    # Click parameters
+    directory = os.path.join(os.getcwd(), 'dicts')
+    output = os.path.join(os.getcwd(), 'dicts.txt')
+
+    click.echo("Generating dict (this can take a long time):")
+    lang_dict = data.cascade_dicts(directory)
+    click.echo("Results:")
+    with open(output, 'w') as file:
+        for key, terms in lang_dict:
+            tokens = terms[:]
+            tokens.expand(key)
+            file.write(' '.join(tokens))
 if __name__ == "__main__":
     main()
